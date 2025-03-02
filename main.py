40801d32a6d2ff4ebfd6255dd5d61da4f50ba611
Andrelin <andrelin.ku@gmail.com>
2025-03-02 19:41:42 +0500

    comment deleted

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import asyncio
 
 from fastapi import FastAPI
-from routers import login
 
 app = FastAPI()
 
-# app.include_router(login.router)
+app.include_router(login.router)
 
 @app.get("/")
 async def root():
