b24a16606c5bfd54db3d1c1e24f95b66e951343a
Andrelin <andrelin.ku@gmail.com>
2025-03-02 19:40:44 +0500

    DELETE THIS COMMENT LATER

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import asyncio
+
 from fastapi import FastAPI
 from routers import login
 
 app = FastAPI()
 
-app.include_router(login.router)
+# app.include_router(login.router)
+
+@app.get("/")
+async def root():
+    await asyncio.sleep(0.2)
+    return {"Hello": "World"}
